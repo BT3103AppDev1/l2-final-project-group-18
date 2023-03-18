42d5c22eb7917e959446660c83b9d7da13254d84
heeeyi <e0772644@u.nus.edu>
2023-03-18 16:35:17 +0800

    Initialise the project skeleton, add a few dummy components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,21 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <DummyTopBar/>
+  <DummyLeftPane/>
+</template>
+
+<script>
+
+import DummyLeftPane from '../components/DummyLeftPane.vue';
+import DummyTopBar from '../components/DummyTopBar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    DummyTopBar,
+    DummyLeftPane
+  }
+}
+
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+
