be80f051ca87126a594a4daec819036507766f8c
yuuutong <97610255+yuuutong@users.noreply.github.com>
2023-03-20 16:15:33 +0800

    added in firebase proj credentials

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,13 @@ import { initializeApp } from 'firebase/app';
 // THIS IS JUST A DUMMY DATABASE WITH TRIAL VALUES
 // CHANGE THIS TO YOUR OWN DATABASE!
 const firebaseConfig = {
-    
+    apiKey: "AIzaSyAQLzRABECiLf3NmSuTA7RfHpjyTURJnus",
+    authDomain: "bt3103-healthbuddy-group18.firebaseapp.com",
+    projectId: "bt3103-healthbuddy-group18",
+    storageBucket: "bt3103-healthbuddy-group18.appspot.com",
+    messagingSenderId: "156269022012",
+    appId: "1:156269022012:web:afcdce03d39b10153cac39",
+    measurementId: "G-X7X1LG8YST"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
