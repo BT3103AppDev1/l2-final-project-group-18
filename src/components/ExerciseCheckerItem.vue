efc10b475712a34a670ddbc03a4c0ae9381ed5f7
heeeyi <e0772644@u.nus.edu>
2023-03-21 21:20:57 +0800

    Implemented basic functionality of searching and displaying for ExerciseChecker

---
+++ src/components/ExerciseCheckerItem.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class = "exercise-checker-item">
+        <div class = "exercise-checker-name">{{ exerciseName }} </div>
+        <div class = "calorie-per-min">{{ caloriePerMinute }} </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: {
+        exerciseName: {
+            type: String,
+            required: true 
+        },
+        caloriePerMinute: {
+            type: Number,
+            required: true
+        }
+    }
+}
+
+</script>
+
+<style>
+.exercise-checker-item {
+    background-color: orange;
+    width: 200px;
+    height: 100px;
+}
+
+</style>
+
