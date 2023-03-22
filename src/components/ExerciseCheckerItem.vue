432d40e432cf7e8b4a9e1606717cb7d9c0874d34
heeeyi <e0772644@u.nus.edu>
2023-03-22 22:02:31 +0800

    Adjust the element styling for Exercise Checker & Pop-up windows

--- src/components/ExerciseCheckerItem.vue
+++ src/components/ExerciseCheckerItem.vue
@@ -25,11 +25,40 @@ export default {
 
 </script>
 
-<style>
+<style scoped>
 .exercise-checker-item {
-    background-color: orange;
-    width: 200px;
+
+    width: 700px;
     height: 100px;
+    background-color: #DDD8BA;
+    border-radius: 20px;
+    margin: 5px;
+}
+
+.exercise-checker-name {
+    position: relative;
+    top: 30px;
+    left: 30px;
+
+    width: 300px;
+    font-family: 'Lato';
+    font-style: normal;
+    font-size: 28px;
+    color: #A08666;
+}
+
+.calorie-per-min {
+    position: relative; 
+    /* You should use relative position, if you want each 
+    entry to adjust based on its current positioning */
+    top: 0px;
+    left: 550px;
+
+    width: 120px;
+    font-family: 'Lato';
+    font-style: normal;
+    font-size: 20px;
+    color: #A08666;
 }
 
 </style>
