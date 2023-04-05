3da8c694c0c608c4436d24d92a776afc1d409cfa
yuuutong <97610255+yuuutong@users.noreply.github.com>
2023-04-05 19:57:17 +0800

    completed 4-quadrant profile page

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,15 +1,17 @@
 <template>
     <profileComponent/>
+    <DummyLeftPane />
 </template>
   
 <script>
   
 import profileComponent from '../components/profileComponent.vue';
-
+import DummyLeftPane from '../components/DummyLeftPane.vue'
   
 export default {
     name: 'App',
     components: {
+        DummyLeftPane,
         profileComponent
     }
 }
