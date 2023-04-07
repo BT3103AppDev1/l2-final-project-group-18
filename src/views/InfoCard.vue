da7c041060feebe66297fa6dfdc7248b0d05c7a3
laughamer <125193093+laughamer@users.noreply.github.com>
2023-04-07 18:18:54 +0800

    Update InfoCard.vue

--- src/views/InfoCard.vue
+++ src/views/InfoCard.vue
@@ -1,19 +1,15 @@
 <template>
-  <FoodTopBar />
-  <FoodTools />
+ 
   <DummyLeftPane />
 </template>
 
 <script>
 import DummyLeftPane from '../components/DummyLeftPane.vue'
-import FoodTools from '../components/FoodTools.vue'
-import FoodTopBar from '../components/FoodTopBar.vue'
 
 export default {
   name: 'App',
   components: {
-    FoodTopBar,
-    FoodTools,
+    
     DummyLeftPane,
   },
 }
