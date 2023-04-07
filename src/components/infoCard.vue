c649b17962c75e5ac5716663299e0df5b9b6b8dd
laughamer <125193093+laughamer@users.noreply.github.com>
2023-04-07 19:40:21 +0800

    Route to infoCard

---
+++ src/components/infoCard.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class = "infoCard">
+        <h1>test</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "infoCard"
+}
+</script>
+
--- src/views/InfoCard.vue
+++ src/views/InfoCard.vue
@@ -1,16 +1,20 @@
 <template>
- 
   <DummyLeftPane />
+  <infoCard />
+
 </template>
 
 <script>
-import DummyLeftPane from '../components/DummyLeftPane.vue'
+import DummyLeftPane from '../components/DummyLeftPane.vue';
+import infoCard from '../components/infoCard.vue';
+
 
 export default {
   name: 'App',
   components: {
-    
     DummyLeftPane,
+    infoCard,
+
   },
 }
 </script>
