dea5ddefadb1cfb7b523479ffca698b6466ef6a6
laughamer <125193093+laughamer@users.noreply.github.com>
2023-04-10 16:05:06 +0800

    infoPage1

---
+++ src/components/infoPage1.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="title">
+    <h1>Top picks for you! </h1>
+    <p>food, exercise, and more!</p>
+  </div>
+</template>
+
+<style scoped>
+.title {
+  position: absolute;
+  width: 953.85px;
+  height: 100px;
+  left: 300px;
+  font-family: "DM Sans";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 35px;
+  line-height: 30px;
+  color: #061428;
+  transform: rotate(0.07deg);
+}
+</style>
