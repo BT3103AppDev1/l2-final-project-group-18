e56802e42f72e71ab3682776f67b4c971ec5641d
win-laeyee <winlae.yee@u.nus.edu>
2023-04-13 20:18:46 +0800

    change eat healthy icon name

--- src/components/signupComponent.vue
+++ src/components/signupComponent.vue
@@ -54,7 +54,7 @@
       "
     />
     <img
-      src="@/assets/Eating-healthy food-cuate.svg"
+      src="@/assets/Eating-healthy-food-cuate.svg"
       alt="heart"
       class="icon"
       style="
